6ca13c1010e6944189999e2d391163341310b450
Francisco Blas (klondike) Izquierdo Riera <klondike@klondike.es>
2020-10-17 08:13:23 +0200

    Never release confidential files

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 bash ./compile.sh &&
 rm -f clipaha.tar.xz &&
-tar --numeric-owner --owner clipaha:0 --group clipaha:0 --mtime=now  -cvJf clipaha.tar.xz benchmark bin clean.sh compile.sh native/*-ref example/ libsodium.js/dist/browsers/sodium.js LICENSE phc-winner-argon2/include/ phc-winner-argon2/src/ src/ ||
+tar --exclude example/server.py --exclude benchmark/download.php --exclude benchmark/save.php --exclude benchmark/db.sql --numeric-owner --owner clipaha:0 --group clipaha:0 --mtime=now  -cvJf clipaha.tar.xz benchmark bin clean.sh compile.sh native/*-ref example/ libsodium.js/dist/browsers/sodium.js LICENSE phc-winner-argon2/include/ phc-winner-argon2/src/ src/ ||
 exit 1
